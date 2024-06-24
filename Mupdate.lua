702ca73710e8172df2f5cf2ca1506ed3127684dc
gesslar <karahd@gmail.com>
2024-06-24 00:34:57 -0400

    removing debugging info

--- Mupdate.lua
+++ Mupdate.lua
@@ -267,7 +267,6 @@ function Mupdate:registerEventHandlers()
     if newEvents["sysDownloadDone"] then
         registerNamedEventHandler(self.package_name, newEvents["sysDownloadDone"], "sysDownloadDone", function(event, path, size, response)
             if not self.in_progress then return end
-            self:Info("We are in progress")
             self:handleDownloadDone(event, path, size, response)
         end)
     end
@@ -275,7 +274,6 @@ function Mupdate:registerEventHandlers()
     if newEvents["sysDownloadError"] then
         registerNamedEventHandler(self.package_name, newEvents["sysDownloadError"], "sysDownloadError", function(event, err, path, actualurl)
             if not self.in_progress then return end
-            self:Info("We are in progress")
             self:handleDownloadError(event, err, path, actualurl)
         end)
     end
@@ -283,7 +281,6 @@ function Mupdate:registerEventHandlers()
     if newEvents["sysGetHttpDone"] then
         registerNamedEventHandler(self.package_name, newEvents["sysGetHttpDone"], "sysGetHttpDone", function(event, url, response)
             if not self.in_progress then return end
-            self:Info("We are in progress")
             self:handleHttpGet(event, url, response)
         end)
     end
@@ -291,7 +288,6 @@ function Mupdate:registerEventHandlers()
     if newEvents["sysGetHttpError"] then
         registerNamedEventHandler(self.package_name, newEvents["sysGetHttpError"], "sysGetHttpError", function(event, response, url)
             if not self.in_progress then return end
-            self:Info("We are in progress")
             self:handleHttpError(event, response, url)
         end)
     end
